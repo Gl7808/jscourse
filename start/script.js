0e0883d45d3c3e40a7c85f58de46e3ae7d82dbdf
unknown <whorshunter42@yandex.ru>
2023-05-11 11:01:34 +0300

    l11 is done

--- start/script.js
+++ start/script.js
@@ -1,11 +1,24 @@
 
-const name = 'vladislav'
-const postsQty =  23
-
-const userProfile = {
-    name : name,
-    postsQty : postsQty,
-    hasSignedAgreement : false
+const button = {
+    widht : 200,
+    text : 'buy',
+    color: 'black'
+}
+const param = {
+    height : 300,
+    material : 'wood'
 }
 
-console.log(userProfile.name)
+const redButton = {
+    ...button,
+    color: 'red'
+}
+
+const item = Object.assign({}, button, param)
+
+console.log(redButton, button, "some text", item)
+console.log(item.height, param.height)
+item.height = 200
+
+console.log(item.height, param.height)
+
