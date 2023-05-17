f5a328aa3f63c5d12037c933e94caf3a4b96e6fd
unknown <whorshunter42@yandex.ru>
2023-05-17 20:26:02 +0300

    start pw lootbox on react

---
+++ reactpw/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './components/navigation/navigation'
+import {Navigation} from "./components/navigation/navigation";
+function App() {
+  return (
+      <div className={'main'}>
+        <Navigation></Navigation>
+      </div>
+  );
+}
+
+export default App;
---
+++ reactpw/src/components/navigation/navigation.tsx
@@ -0,0 +1,12 @@
+import './navigation.css'
+export const Navigation = () => {
+    return(
+        <div>
+            <div className={'navigation'}>
+                <a href={'#'}>Главная</a>
+                <a href={'#'}>Коробки</a>
+                <a href={'#'}>Заточка</a>
+            </div>
+        </div>
+    );
+}
